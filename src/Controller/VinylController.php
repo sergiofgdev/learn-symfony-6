5491147cf9911fe833f1ff3879f694c102101fcd
Sergio Fernandez Garcia <sergio.fernandez@netcheck.es>
2025-02-13 19:32:10 +0100

    Chapter 4

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -4,14 +4,25 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use function Symfony\Component\String\u;
 
 class VinylController
 {
 
     #[Route('/')]
-    public function homepage()
+    public function homepage() : Response
     {
         return new Response("I am Sergio");
+    }
+
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null ) : Response
+    {
+        $title = "All genres";
+        if ($slug) {
+            $title = 'Genre: ' . u(str_replace("-", " ", $slug))->title(true);
+        }
+        return new Response($title);
     }
 
 }
