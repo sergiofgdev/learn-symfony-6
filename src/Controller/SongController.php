dd7ea78c4b8fa788b05a0b9b0b48ba761337beb6
Sergio Fernandez <sergiofg.dev@proton.me>
2025-02-14 10:40:37 +0100

    Chapter 12

---
+++ src/Controller/SongController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class SongController extends AbstractController
+{
+    #[Route('/api/songs/{id}')]
+    public function getSong($id): Response
+    {
+        $song = [
+            'id' => $id,
+            'name' => 'Song 1',
+            'url' => 'https://www.google.com/',
+        ];
+
+//        return new JsonResponse(($song));
+        return $this->json($song);
+    }
+
+}
