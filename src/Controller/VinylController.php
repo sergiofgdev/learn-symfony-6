ee240c90b068a53a0e7a39a78c5f2446841454fe
Sergio Fernandez <sergiofg.dev@proton.me>
2025-02-15 12:54:14 +0100

    Symfony 6 Fundamentals: Services, Config & Environments Chapters 1-4

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -4,40 +4,41 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
-use Twig\Environment;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
 {
-
     #[Route('/', name: 'app_homepage')]
     public function homepage(): Response
     {
         $tracks = [
-            ['song' => 'Gangs Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
             ['song' => 'Waterfalls', 'artist' => 'TLC'],
             ['song' => 'Creep', 'artist' => 'Radiohead'],
             ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
-            ['song' => 'On bended knee', 'artist' => 'Boyz II Men'],
-            ['song' => 'Fantasy', 'artist' => 'Mariah Carey']
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
         ];
 
-        return $this->render('/vinyl/homepage.html.twig', [
-            'title' => 'Homepage',
-            'tracks' => $tracks
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $tracks,
         ]);
     }
 
     #[Route('/browse/{slug}', name: 'app_browse')]
-    public function browse(string $slug = null): Response
+    public function browse(HttpClientInterface $httpClient, ?string $slug = null): Response
     {
-        $genre = $slug ? u(str_replace("-", " ", $slug))->title(true) : null;
+        $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
+        $response = $httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/refs/heads/main/mixes.json');
+        $mixes = $response->toArray();
 
 
-        return $this->render('/vinyl/browse.html.twig', [
+        return $this->render('vinyl/browse.html.twig', [
             'genre' => $genre,
+            'mixes' => $mixes
         ]);
     }
-
 }
