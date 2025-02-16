3f44f2ca417d29269dc38bbd90e1fa811d30fdcf
Sergio Fernandez <sergiofg.dev@proton.me>
2025-02-16 12:50:51 +0100

    Chapter 14

--- src/Service/MixRepository.php
+++ src/Service/MixRepository.php
@@ -9,22 +9,22 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 class MixRepository
 {
 
-    private $httpClient;
-    private $cache;
 
-    public function __construct(HttpClientInterface $httpClient, CacheInterface $cache, ){
-        $this->httpClient = $httpClient;
-        $this->cache = $cache;
+    public function __construct(
+        private HttpClientInterface $httpClient,
+        private CacheInterface      $cache,
+        private bool                $isDebug)
+    {
+
     }
+
     public function findAll()
     {
-        $mixes = $this->cache->get('mixes_data', function (CacheItemInterface $cacheItem)  {
-            $cacheItem->expiresAfter(5);
+        return $this->cache->get('mixes_data', function (CacheItemInterface $cacheItem) {
+            $cacheItem->expiresAfter($this->isDebug ? 5 : 60);
             $response = $this->httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/refs/heads/main/mixes.json');
             return $response->toArray();
         });
-
-        return $mixes;
 
     }
 }
