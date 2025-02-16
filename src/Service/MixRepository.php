1bbaf577b24c18cf98abd6dc56a32c870229d420
Sergio Fernandez <sergiofg.dev@proton.me>
2025-02-16 14:12:53 +0100

    Chapter 18

--- src/Service/MixRepository.php
+++ src/Service/MixRepository.php
@@ -3,6 +3,9 @@
 namespace App\Service;
 
 use Psr\Cache\CacheItemInterface;
+use Symfony\Bridge\Twig\Command\DebugCommand;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
@@ -15,13 +18,22 @@ class MixRepository
         private HttpClientInterface $githubContentClient,
         private CacheInterface      $cache,
         #[Autowire('$kernel.debug%')]
-        private bool                $isDebug)
+        private bool                $isDebug,
+        #[Autowire(service: 'twig.command.debug')]
+        private DebugCommand        $twigDebugCommand,
+    )
     {
 
     }
 
     public function findAll()
     {
+
+//        $output = new BufferedOutput();
+//        $this->twigDebugCommand->run(new ArrayInput([]), $output);
+//        dd($output);
+
+
         return $this->cache->get('mixes_data', function (CacheItemInterface $cacheItem) {
             $cacheItem->expiresAfter($this->isDebug ? 5 : 60);
             $response = $this->githubContentClient->request('GET', '/SymfonyCasts/vinyl-mixes/refs/heads/main/mixes.json');
