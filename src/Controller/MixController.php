99e3ff7ab9c53ff10aecf9a9c311b30364405214
Sergio Fernandez <sergiofg.dev@proton.me>
2025-02-17 15:58:47 +0100

    Chapter 14

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -6,6 +6,7 @@ use App\Entity\VinylMix;
 use App\Repository\VinylMixRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -39,7 +40,7 @@ class MixController extends AbstractController
         ));
     }
 
-    #[Route('/mix/{id}',name: 'app_mix_show')]
+    #[Route('/mix/{id}', name: 'app_mix_show')]
     public function show(VinylMix $mix)
     {
         //Esto no es necesario si pasamos como argumento VinylMix, es util cuando necesitamos un simple objeto
@@ -50,8 +51,6 @@ class MixController extends AbstractController
 //        }
 
 
-
-
 //        dd($mix);
         return $this->render('mix/show.html.twig', [
             'mix' => $mix
@@ -59,4 +58,17 @@ class MixController extends AbstractController
     }
 
 
+    #[Route('mix/{id}/vote', name: 'app_mix_vote', methods: ['POST'])]
+    public function vote(VinylMix $mix, Request $request): Response
+    {
+        $miReq = $request;
+//        dd($miReq);
+
+        $direction = $request->request->get('direction','up');
+        if($direction == 'up'){
+            $mix->setVotes($mix->getVotes() + 1);
+        }else{
+            $mix->setVotes($mix->getVotes() - 1);
+        }
+    }
 }
